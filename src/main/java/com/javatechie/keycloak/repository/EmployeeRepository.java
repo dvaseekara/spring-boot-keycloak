825de6071cfd6aedc8203ea4c566bd73121e8b4b
Daniel Vaseekaran <dvaseeka@redhat.com>
2023-04-17 10:52:33 -0400

    Updated packages

--- src/main/java/com/javatechie/keycloak/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javatechie.keycloak.repository;
-
-import com.javatechie.keycloak.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-}
