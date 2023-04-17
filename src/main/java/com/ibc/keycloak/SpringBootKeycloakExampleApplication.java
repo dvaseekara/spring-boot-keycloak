880639d69474e5764c0b7e358afae490c946635d
Daniel Vaseekaran <dvaseeka@redhat.com>
2023-04-17 11:51:22 -0400

    Cleaned up endpoints

--- src/main/java/com/ibc/keycloak/SpringBootKeycloakExampleApplication.java
+++ src/main/java/com/ibc/keycloak/SpringBootKeycloakExampleApplication.java
@@ -1,45 +1,33 @@
 package com.ibc.keycloak;
 
-import com.ibc.keycloak.entity.Employee;
-import com.ibc.keycloak.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
-import java.util.List;
 
 @SpringBootApplication
 @RestController
 @RequestMapping("/")
 public class SpringBootKeycloakExampleApplication {
-
-    @Autowired
-    private EmployeeService service;
-
     //this method can be accessed by user whose role is user
-    @GetMapping("/{employeeID}")
-    @RolesAllowed("user")
-    public ResponseEntity<Employee> getEmployee(@PathVariable int employeeId) {
-        return ResponseEntity.ok(service.getEmployee(employeeId));
-    }
+	public String home() {
+		return "Greetings from Spring Boot Service A!";
+	}
 
-    //this method can be accessed by user whose role is admin
-    @GetMapping("/admin")
+	@GetMapping("/users")
+	@RolesAllowed("user")
+	public String users() {
+		return "Greetings User!";
+	}
+
+	@GetMapping("/users")
     @RolesAllowed("admin")
-    public ResponseEntity<List<Employee>> findALlEmployees() {
-        return ResponseEntity.ok(service.getAllEmployees());
-    }
-
-    @GetMapping
-    public String home() {
-        return "Joie de vivre is a French phrase often used in English to express a cheerful enjoyment of life, an exultation of spirit.";
-    }
+	public String admins() {
+		return "Greetings Admin!";
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootKeycloakExampleApplication.class, args);
--- src/main/java/com/ibc/keycloak/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ibc.keycloak.repository;
-
-import com.ibc.keycloak.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-}
